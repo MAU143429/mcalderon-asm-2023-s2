26181da1ac8901f9c7504551bf75acde9fcbb014
MAU <mau141226@gmail.com>
2023-09-17 21:31:16 -0600

    [Add] Spectrogram methods added

---
+++ Spectrograms.py
@@ -0,0 +1,47 @@
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Cargar un archivo de audio
+audio_file = 'sample.wav'
+y, sr = librosa.load(audio_file)
+
+# Calcular el espectrograma de magnitud
+D = librosa.stft(y)
+mag = librosa.amplitude_to_db(abs(D))
+
+# Calcular el espectrograma de potencia
+power = np.abs(D)**2
+
+# Calcular el espectrograma de Mel
+S = librosa.feature.melspectrogram(y=y, sr=sr)
+log_S = librosa.power_to_db(S, ref=np.max)
+
+# Crear una figura con tres subplots
+plt.figure(figsize=(12, 8))
+
+# Espectrograma de magnitud (Armónicos)
+plt.subplot(3, 1, 1)
+librosa.display.specshow(mag, sr=sr, x_axis='time', y_axis='log',cmap='viridis')
+plt.colorbar(format='%+2.0f dB')
+plt.title('Espectrograma de Magnitud (Armónicos)')
+
+# Espectrograma de potencia (Transitorios)
+plt.subplot(3, 1, 2)
+librosa.display.specshow(librosa.power_to_db(power, ref=np.max), sr=sr, x_axis='time', y_axis='log',cmap='viridis')
+plt.colorbar(format='%+2.0f dB')
+plt.title('Espectrograma de Potencia (Transitorios)')
+
+# Espectrograma de Mel (Timbre)
+plt.subplot(3, 1, 3)
+librosa.display.specshow(log_S, sr=sr, x_axis='time', y_axis='mel',cmap='viridis')
+plt.colorbar(format='%+2.0f dB')
+plt.title('Espectrograma de Mel (Timbre)')
+
+# Ajustar los subplots
+plt.tight_layout()
+
+# Mostrar el gráfico
+plt.show()
+
